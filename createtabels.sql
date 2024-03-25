2069a0dfd1df2683c9d6fd551b30f7ea31b370bf
Babakaaz <Babakaaz@users.noreply.github.com>
2024-03-25 20:55:59 +0430

    added foreign key constraints to the create script

--- createtabels.sql
+++ createtabels.sql
@@ -1,5 +1,5 @@
 PRAGMA encoding = 'UTF-8';
---creating table
+PRAGMA foreign_keys = OFF;
 CREATE TABLE IF NOT EXISTS users (
 	userid 			INTEGER 	PRIMARY KEY,
 	username 		TEXT 		NOT NULL	 UNIQUE,
@@ -7,38 +7,43 @@ CREATE TABLE IF NOT EXISTS users (
 	phone 			TEXT 		NOT NULL  	 UNIQUE,
 	passhash 		TEXT,
 	passsalt 		TEXT ,
-	creation_date   INTEGER,
-	lastname	    TEXT ,
+	creation_date   	INTEGER,
+	lastname	    	TEXT ,
 	firstname 		TEXT ,
 	online 			INTEGER 	NOT NULL  DEFAULT 1,
+	status			INTEGER,
 	CHECK( online < 2 AND
 	 length(phone) == 11 ));
 CREATE TABLE IF NOT EXISTS items (
 	itemid 			INTEGER   PRIMARY KEY,
 	price 			INTEGER	  NOT NULL,
 	category 		TEXT			  ,
-	name	   	    TEXT 	  NOT NULL,
-	number_left 	INTEGER   NOT NULL, 
-	creation_date   INTEGER   NOT NULL,
+	name	   	    	TEXT 	  NOT NULL,
+	number_left 		INTEGER   NOT NULL, 
+	creation_date   	INTEGER   NOT NULL,
 	rating 			INTEGER,
-	CHECK ( price != 0 AND
+	ratingnum		INTEGER,
+	status			INTEGER, CHECK ( price != 0 AND
 	 rating < 6 )
 	);
 CREATE TABLE IF NOT EXISTS attributes (
 	itemid  INTEGER 		 PRIMARY KEY,
 	key 	TEXT,
-	value 	TEXT
+	value 	TEXT,
+	FOREIGN KEY (itemid) REFERENCES items (itemid) ON DELETE NO ACTION
 	);
 CREATE TABLE IF NOT EXISTS basketlist (
 	userid 			INTEGER		PRIMARY KEY,
 	basketid 		INTEGER     NOT NULL UNIQUE,
-	creation_date   INTEGER 	NOT NULL,
-	ispurchased 	INTEGER 	DEFAULT 0
-	--check for basketlist
-	CHECK ( ispurchased < 2 )
+	creation_date   	INTEGER 	NOT NULL,
+	ispurchased 		INTEGER 	DEFAULT 0
+	CHECK ( ispurchased < 2 ),
+	FOREIGN KEY (userid) REFERENCES users(userid) ON DELETE NO ACTION,
+	FOREIGN KEY (basketid) REFERENCES basket(basketid) ON DELETE NO ACTION
 	);
 CREATE TABLE IF NOT EXISTS basket (
-	basketid 	INTEGER		PRIMARY KEY,
-	itemid 		INTEGER		NOT NULL,
-	countbasket INTEGER		NOT NULL
+	basketid 		INTEGER		PRIMARY KEY,
+	itemid 			INTEGER		NOT NULL,
+	itemcount 			INTEGER		NOT NULL,
+	FOREIGN KEY (itemid) REFERENCES items (itemid) ON DELETE NO ACTION
 	);
