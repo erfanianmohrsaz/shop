c8f5beb1487252b06a82cad4e4d9bf3a62f16c63
Alex <138152554+AliGhanizade@users.noreply.github.com>
2024-03-26 15:38:38 +0330

    Add login and Sing
    Add first file "login" and "Sing in" in view folder

---
+++ view/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <a href="login.php" target="_blank" rel="noopener noreferrer"><input type="button"  value="Login"></a>
+   <a href="register.php" target="_blank" rel="noopener noreferrer"><input type="button"  value="Sing in"></a>
+</body>
+</html>
