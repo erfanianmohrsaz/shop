12a0c1e239812195d0e85b1cc8e971a3ced156e3
Alex <138152554+AliGhanizade@users.noreply.github.com>
2024-03-27 17:56:53 +0330

    debug for line 5

--- view/register.php
+++ view/register.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <!-- Add file -->
-    <?php require  Lib/lib-login.php ?>
+    <?php require  "../Lib/lib-login.php" ?>
     
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
