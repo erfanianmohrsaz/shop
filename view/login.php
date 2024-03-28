5edaeb06805146fbbcb531d57908733674d9ae0f
Babakaaz <Babakaaz@users.noreply.github.com>
2024-03-28 05:30:15 +0430

    restructured the layout of some of the files

--- view/login.php
+++ view/login.php
@@ -8,27 +8,29 @@
 
 </head>
 <body dir="rtl">
-<!-- partial:index.partial.html -->
+
 <div class="container">
   <div class="login-box">
     <h2>ورود</h2>
-    <form method=post>
+    <form action="#">
       <div class="input-box">
-        <input name=user type="text" required>
+        <input type="text" required>
         <label>نام کاربری</label>
       </div>
       <div class="input-box">
-        <input name=pass type="password" required>
+        <input type="password" required>
         <label>رمز</label>
       </div>
-    
+    <div>
+      <label class="danger">پسورد درست نیست</label>
+    </div>
       <button type="submit" class="btn">ورود</button>
       <div class="signup-link">
         <a href="sign-up.html">ساخت حساب</a>
       </div>
     </form>
   </div>
-<!-- partial -->
+
   
 </body>
 </html>
